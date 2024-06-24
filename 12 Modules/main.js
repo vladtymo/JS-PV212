7f13413659570d2d2f0313a35f90ac9a62e1f6d9
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-06-24 18:58:49 +0300

    Modules

---
+++ 12 Modules/main.js
@@ -0,0 +1,17 @@
+// ----------- modules -----------
+// we can use items from different files (modules)
+// keywords: [import] [export]
+
+// we can [import] only exported items
+import { id as identity, getRandom, User } from './utils.js';
+//import * as utils from './utils.js'
+
+console.log("ID:", identity);
+//console.log("User:", user); // not imported
+console.log(getRandom(1, 5));
+
+const my = new User("blablauser");
+
+my.print();
+
+// Formatting: [Ctrl + K + F]
